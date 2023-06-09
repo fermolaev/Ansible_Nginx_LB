fc99b4fd420d5b6424ad8b00ef2cff2b10e56a13
Fedor Ermolaev <len1256@mail.ru>
2023-06-09 14:57:50 +0300

    Nginx+Let's Encrypt every month

--- resources.tf
+++ resources.tf
@@ -13,9 +13,8 @@ data "digitalocean_ssh_key" "rebrain" {
   name = "REBRAIN.SSH.PUB.KEY"
 }
 
-resource "digitalocean_ssh_key" "myssh" {
-  name       = "SSH Key Terraform 2"
-  public_key = file(var.ssh_path)
+data "digitalocean_ssh_key" "myssh" {
+  name = "SSH Key Terraform 2"
 }
 
 resource "digitalocean_droplet" "vm" {
@@ -25,7 +24,7 @@ resource "digitalocean_droplet" "vm" {
   name     = var.devs[count.index]
   region   = var.region
   size     = var.vm_size
-  ssh_keys = [data.digitalocean_ssh_key.rebrain.id, digitalocean_ssh_key.myssh.fingerprint]
+  ssh_keys = [data.digitalocean_ssh_key.rebrain.id, data.digitalocean_ssh_key.myssh.id]
   tags     = var.task_email
 
   #Подключение в создаваемой VM для установки пароля 
@@ -110,7 +109,7 @@ resource "digitalocean_droplet" "ansible" {
   name       = "ansible"
   region     = var.region
   size       = var.vm_size
-  ssh_keys   = [digitalocean_ssh_key.myssh.fingerprint]
+  ssh_keys   = data.digitalocean_ssh_key.myssh.id
   tags       = var.task_email
 
   connection {
@@ -144,4 +143,4 @@ resource "digitalocean_droplet" "ansible" {
 
 }
 
-#"ansible-playbook nginx-instal.yml -i /root/hosts.yaml  --ssh-common-args='-o StrictHostKeyChecking=no'",
+#"ansible-playbook role_nginx.yml -i /root/hosts.yaml  --ssh-common-args='-o StrictHostKeyChecking=no'",
